dbfb98e7157877ed70f9cebef438f30ddf10309f
fgandraf <fgandraf@gmail.com>
2022-12-27 15:51:41 -0300

    Listas em python

--- Program.py
+++ Program.py
@@ -1,5 +1,24 @@
+""" CONTADOR
 contador = 0
 while contador < 10:
     if contador > 5:
         print (contador)
     contador = contador + 1
+"""
+
+print("\x1b[2J\x1b[1;1H")
+
+lista = ['a', 3, True, 4.5]
+print(lista)
+
+lista.append(45)
+print(lista)
+
+lista.insert(0,56)
+print(lista)
+
+lista.pop()
+print(lista)
+
+lista.clear()
+print(lista)
